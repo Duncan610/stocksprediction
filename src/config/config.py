66c47089b1abe0bfe687fed8ecee081bb9e5e985
Duncan Otieno <otienoduncan99@gmail.com>
2025-03-10 20:53:23 +0300

    Updated config.py to load API key from .env file

--- src/config/config.py
+++ src/config/config.py
@@ -1,9 +1,17 @@
 import os
+from dotenv import load_dotenv
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-DATA_DIR = os.path.join(BASE_DIR, "data", "raw")
-API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+# Load .env file
+load_dotenv()
+
+# Paths
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "data")
+MODEL_DIR = os.path.join(DATA_DIR, "models")
+PROCESSED_DIR = os.path.join(DATA_DIR, "processed")
+RAW_DIR = os.path.join(DATA_DIR, "raw")
+
+# Alpha Vantage API
 ALPHA_VANTAGE_URL = "https://www.alphavantage.co/query"
-
+API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")
 if not API_KEY:
     raise ValueError("ALPHA_VANTAGE_API_KEY environment variable not set.")
